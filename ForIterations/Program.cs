357566df5ea28cfec125e3143e99ab16c2439fee
jverratti <31357738+jverratti@users.noreply.github.com>
2017-09-11 19:16:53 -0600

    ForIterations

---
+++ ForIterations/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ForIterations
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                //Console.WriteLine(i.ToString());
+
+                if(i == 7)
+                {
+                    Console.WriteLine("Found seven!");
+                    break;
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
+//for loops are used to run a program until a 
+//certain result is hit
+//i++ == i + 1
